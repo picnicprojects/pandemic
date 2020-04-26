2a22f9751ba702102d8a746b226ab9e2da4a077c
kweert <clmvanweert@gmail.com>
2020-04-26 07:51:36 +0200

    slower and better

--- pandemic.py
+++ pandemic.py
@@ -5,28 +5,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 HEALTHY = 0
-CONTAGIOUS = 1
-IMMUNE = 2
-color = ['b.','r.','g.']
+INFECTED = 1
+CONTAGIOUS = 2
+IMMUNE = 3
+color = ['b.','m.','r.','g.']
 
 class Person():
    def __init__(self,i):
       # Public
       self.index = i
-      self.age = 30
       self.state = HEALTHY
-      self.T_incubation = 8 #days
-      self.T0 = 0 # infection day
+      self.T_infected = 5 #days
+      self.T_contagious = 20 #days
       self.x = random.random()
       self.y = random.random()
       
       # Private 
-      self._vx = 0.1*(random.random()-0.5)
-      self._vy = 0.1*(random.random()-0.5)
+      self._vx = 0.01*(random.random()-0.5)
+      self._vy = 0.01*(random.random()-0.5)
       
    def move(self):
-      self.x = self.x + self._vx*random.random()
-      self.y = self.y + self._vy*random.random()
+      self.x = self.x + self._vx
+      self.y = self.y + self._vy
       if self.x > 1 or self.x < 0:
          self._vx = -self._vx
       if self.y > 1 or self.y < 0:
@@ -34,10 +34,11 @@ class Person():
       self.x = max(min(self.x, 1),0)
       self.y = max(min(self.y, 1),0)
 
-   def check_immunity(self, day):
-      if (self.state == CONTAGIOUS) and (day > (self.T0 + self.T_incubation)):
+   def change_state(self, day):
+      if (self.state == INFECTED)   and (day > (self.T0 + self.T_infected)):
+         self.state = CONTAGIOUS
+      if (self.state == CONTAGIOUS) and (day > (self.T0 + self.T_infected + self.T_contagious)):
          self.state = IMMUNE
-
       
 class DistanceMatrix():
    def __init__(self,n):
@@ -57,19 +58,9 @@ class DistanceMatrix():
       a = []
       for i in range(self._n):
          for j in range(self._n):
-            if (distance[i,j] < 0.08) and (i != j):
+            if (distance[i,j] < 0.02) and (i != j):
                a.append([i,j])
       return(a)         
-      
-class ContactMatrix():
-   def __init__(self,n):
-      self.n = n
-      self.distance_matrix = np.zeros((n,n))
-      self.contact_matrix = np.zeros((n,n))
-      
-   def update(self,i,x,y):
-      pass
-      # self.distance_matrix =  
       
 class Pandemic():
    def __init__(self):
@@ -78,31 +69,36 @@ class Pandemic():
       self.person = []
       for i in range(self.n):
          self.person.append(Person(i))
-      self.person[0].state = CONTAGIOUS
+      self.person[0].state = INFECTED
+      self.person[0].T0 = 0
    
    def run(self):
       d = DistanceMatrix(self.n)
-      for day in range(self.T_duration):
+      steps_per_day = 10
+      for step in range(self.T_duration * steps_per_day):
+         day = step / steps_per_day;
          # Calculate
          for person in self.person:
             person.move()
-            person.check_immunity(day)
+            person.change_state(day)
             d.update(person.index,person.x,person.y)
          hits = d.get_hits()
          for i,j in hits:
             if (self.person[i].state == HEALTHY) and (self.person[j].state == CONTAGIOUS):
-               self.person[i].state = CONTAGIOUS
+               self.person[i].state = INFECTED
                self.person[i].T0 = day
             if (self.person[j].state == HEALTHY) and (self.person[i].state == CONTAGIOUS):
-               self.person[j].state = CONTAGIOUS
+               self.person[j].state = INFECTED
                self.person[j].T0 = day
 
          # Plot
-         for person in self.person:
-            if hasattr(person, 'previous'):
-               person.previous.remove()
-            person.previous, = plt.plot(person.x,person.y,color[person.state])
-         plt.pause(0.001)
+         if day == int(day):
+            for person in self.person:
+               if hasattr(person, 'previous'):
+                  person.previous.remove()
+               person.previous, = plt.plot(person.x,person.y,color[person.state])
+            plt.title('day %d' % day)
+            plt.pause(0.000001)
       plt.show()
             
 if __name__ == '__main__':
